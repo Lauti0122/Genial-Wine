599f7ab67b7c0bc499254fc9471b3968c3e1f22e
LichiLandaburu <lichilanda@gmail.com>
2023-01-17 17:40:25 -0300

    initial commit

---
+++ client/src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNsrMz2k9AGEPgtt1bGgfJxWaVioefW0M",
+  authDomain: "genial-wine.firebaseapp.com",
+  projectId: "genial-wine",
+  storageBucket: "genial-wine.appspot.com",
+  messagingSenderId: "289085683226",
+  appId: "1:289085683226:web:a08fcba7e75d4c6975c3a3",
+  measurementId: "G-W6Z60K6FMP"
+};
+
+// Initialize Firebase
+export const initiFirestore = initializeApp(firebaseConfig);
+export const db = getFirestore(initiFirestore);
