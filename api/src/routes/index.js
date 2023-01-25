0df9b8cbe8c8ef8516f499dce859525e6f938b66
LichiLandaburu <lichilanda@gmail.com>
2023-01-25 12:08:05 -0300

    mercadopago

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const users = require("./user.routes");
 const wines = require("./wine.routes");
 const orders = require("./order.routes");
+const payment = require("./payment.routes");
 
 module.exports = router;
 
@@ -12,3 +13,4 @@ router.use(express.json());
 router.use("/auth", users);
 router.use("/wines", wines);
 router.use("/orders", orders);
+router.use("/payment", payment);
